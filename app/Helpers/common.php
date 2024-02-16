92538b7fd9b741de8176034527c542814058db80
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-02-16 16:26:39 +0400

    first changes

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -1,18 +1,5 @@
 <?php
 
-// function customUserResource($data)
-// {
-//     return [
-//         'id' => $data->id,
-//         'name' => $data->name,
-//         'avatar' => $data->avatar,
-//         'passport' => $data->passport,
-//         'phone' => $data->phone,
-//         'created_at' => $data->created_at,
-//         'updated_at' => $data->updated_at,
-//     ];
-// }
-
 function translateMessageApi($message, $lang=null)
 {
     $lang = $lang ? $lang : session('languages', 'am');
@@ -52,4 +39,17 @@ if(!function_exists('languages')){
       ];
 
   }
+}
+
+if(!function_exists('languagesName')){
+    function languagesName($key){
+        $arr = [
+            'am' => 'հայերեն',
+            'ru' => 'ռուսերեն',
+            'en' => 'անգլերեն',
+        ];
+        
+        return $arr[$key];
+
+    }
 }
