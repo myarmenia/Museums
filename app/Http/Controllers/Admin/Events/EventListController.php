2a7a8cc493e54a05789522d3dddebcdb02a71e43
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-03-13 14:25:00 +0400

    check_event_and_museum_branch_api

--- app/Http/Controllers/Admin/Events/EventListController.php
+++ app/Http/Controllers/Admin/Events/EventListController.php
@@ -8,15 +8,22 @@ use Illuminate\Http\Request;
 
 class EventListController extends Controller
 {
+  public function __construct(){
+
+    $this->middleware('role:museum_admin');
+
+  }
   public function __invoke(Request $request){
-    $data = Event::where('id','>',0)
-    // return view('content.event.index',compact("data"));
-    ->orderBy('id', 'DESC')->paginate(2)->withQueryString();
+    $data = Event::where([
+                  ['id','>',0],
+                  ['museum_id','=',museumAccessId()]
+                  ])
+    ->orderBy('id', 'DESC')->paginate(10)->withQueryString();
     return view('content.event.index', [
         'data' => $data,
 
     ])
-         ->with('i', ($request->input('page', 1) - 1) * 2);
+         ->with('i', ($request->input('page', 1) - 1) * 10);
   }
 
 }
