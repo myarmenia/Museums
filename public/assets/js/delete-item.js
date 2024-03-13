aa4da8da793ea2459c5d46548138aecf915b08ec
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-03-13 10:04:17 +0400

    remove alert and log in delete item

--- public/assets/js/delete-item.js
+++ public/assets/js/delete-item.js
@@ -3,10 +3,9 @@ $(function () {
     let id = $(this).parents('.action').attr('data-id')
     let tb_name = $(this).parents('.action').attr('data-tb-name')
     let url = `/delete-item/${tb_name}/${id}`
-console.log(id, tb_name, url)
+
     $('.delete_item').attr('data-url', url)
     $('.message_cont').html('')
-    alert(6)
 
   })
 })
@@ -14,7 +13,6 @@ console.log(id, tb_name, url)
 
 $(function () {
   $('.delete_item').on('click', function () {
-    alert()
     let url = $(this).attr('data-url')
     let id = url.split("/").pop();
     let row = $(`.action[data-id="${id}"]`).parents('tr')
