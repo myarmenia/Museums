4a651ac40d5cdfd3fa30e63375aa571093129ea1
NarineHay <naromisho87@gmail.com>
2024-04-02 11:16:16 +0400

    5566

--- resources/views/content/reports/super-admin.blade.php
+++ resources/views/content/reports/super-admin.blade.php
@@ -19,21 +19,12 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
 <script>
-<<<<<<< HEAD
-      $( '.select-2' ).select2( {
-    theme: "bootstrap-5",
-    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-    placeholder: $( this ).data( 'placeholder' ),
-    closeOnSelect: false,
-} );
-=======
       $( '.select-2').select2( {
           theme: "bootstrap-5",
           width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
           placeholder: $( this ).data( 'placeholder' ),
           closeOnSelect: false,
       } );
->>>>>>> b7164eada7e1e5e6e819c41c3ac50b8d1d39b173
 </script>
 
 @endsection
@@ -62,16 +53,9 @@
             <div>
                 <form action="{{route('logs')}}" method="get" class="row g-3 mt-2" style="display: flex">
                     <div class="mb-3 justify-content-end" style="display: flex; gap: 8px">
-
-
-                        <div class="col-2">
+                       <div class="col-2">
                             {{-- <select id="defaultSelect" name="museum_id" class="form-select" value="{{ request()->input('type') }}" > --}}
-<<<<<<< HEAD
-                              <select class="form-select select-2" id="multiple-select-museum" data-placeholder="Թանգարան" multiple>
-                                    {{-- <option value="" disabled selected>Թանգարան</option> --}}
-=======
                               <select class="form-select select-2" id="multiple-select-museum" data-placeholder="Թանգարան" name="museum_id" multiple>
->>>>>>> b7164eada7e1e5e6e819c41c3ac50b8d1d39b173
                                     @foreach ($museums as $museum)
                                         <option value="{{$museum->id}}" {{ request()->input('museum_id') == $museum->id ? 'selected' : '' }}>{{$museum->translationsForAdmin->name}}</option>
                                     @endforeach
@@ -79,18 +63,65 @@
                             </select>
                         </div>
                         <div class="col-2">
-<<<<<<< HEAD
-                            <select id="multiple-select-type" name="type" class="form-select select-2" value="{{ request()->input('type') ?? ''}}" data-placeholder="Հաշվետվության տեսակ">
-=======
                             <select id="multiple-select-type" name="type" class="form-select select-2" data-placeholder="Հաշվետվության տեսակ" value="{{ request()->input('type') ?? ''}}" >
-                                {{-- <option disabled selected>Հաշվետվության տեսակ</option> --}}
->>>>>>> b7164eada7e1e5e6e819c41c3ac50b8d1d39b173
+                                <option disabled selected>Հաշվետվության տեսակ</option>
                                 <option value="financial" >Ֆինասական</option>
                                 <option value="quantitative" >Քանակական</option>
                                 <option value="fin_quant" >Քանակ/Ֆին</option>
                             </select>
                         </div>
 
+                        <div class="col-2">
+                            <select id="multiple-select-payment_method" name="payment_method" class="form-select select-2" data-placeholder="Վճարման եղանակ" value="{{ request()->input('payment_method') ?? ''}}" >
+                                <option disabled selected>Վճարման եղանակ</option>
+                                <option value="online" >Առցանց</option>
+                                <option value="cash_box" >Դրամարկղ</option>
+
+                            </select>
+                        </div>
+
+                        <div class="col-1">
+                            <select id="multiple-select-gender" name="gender" class="form-select select-2" data-placeholder="Սեռ" value="{{ request()->input('gender') ?? ''}}" >
+                                <option disabled selected>Սեռ</option>
+                                <option value="male" >Արական</option>
+                                <option value="female" >Իգական</option>
+                                <option value="unknown" >Անհայտ</option>
+                            </select>
+                        </div>
+
+                        <div class="col-2">
+                            <select id="multiple-select-age" name="age" class="form-select select-2" data-placeholder="Տարիք" value="{{ request()->input('age') ?? ''}}" >
+                                <option disabled selected>Տարիք</option>
+                                <option value="0-12" >մինչև 12</option>
+                                <option value="13-18" >13 - 18</option>
+                                <option value="19-60" >19 - 60</option>
+                                <option value="61" >61 և ավել</option>
+
+                            </select>
+                        </div>
+
+                        <div class="col-2">
+                            <select id="multiple-select-country" name="country_id" class="form-select select-2" data-placeholder="Երկիր" value="{{ request()->input('country_id') ?? ''}}" >
+                                <option disabled selected>Երկիր</option>
+                                @foreach (getAllCountries() as $country)
+                                    <option value="{{$country->id}}" {{ request()->input('country_id') == $country->id ? 'selected' : '' }}>{{$country->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mb-3 justify-content-end" style="display: flex; gap: 8px">
+{{-- <div class="col-2 d-flex">
+                        <div class="">
+                          <label for="datefrom">datefrom</label>
+                            <input type="date" class="form-control" id="datefrom" placeholder="Ստեղծման ամսաթիվ" name="from_created_at" value="{{ request()->input('from_created_at') }}">
+                        </div>
+
+                        <div class="">
+                          <label for="dateto">dateto</label>
+
+                            <input type="date" class="form-control" id="dateto" placeholder="Ստեղծման ամսաթիվ" name="to_created_at" value="{{ request()->input('to_created_at') }}">
+                        </div>
+</div> --}}
                         <div class="col-2">
                             <select id="multiple-select-time" name="time" class="form-select select-2" value="{{ request()->input('time') ?? ''}}" data-placeholder="Ժամանակահատված" multiple>
                                 <option value="first_trimester" >1 եռամսյակ</option>
