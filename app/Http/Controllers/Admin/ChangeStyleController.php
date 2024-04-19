4deb657793f6e09b24b2d1b6920a394bc8eb4d67
NarineHay <naromisho87@gmail.com>
2024-04-19 17:54:45 +0400

    confirmed

--- app/Http/Controllers/Admin/ChangeStyleController.php
+++ app/Http/Controllers/Admin/ChangeStyleController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\TicketQr;
+use App\Traits\NodeApi\QrTokenTrait;
 use Illuminate\Http\Request;
 use App\Mail\SendQRTiketsToUsersEmail;
 use Mail;
 class ChangeStyleController extends Controller
 {
+  use QrTokenTrait;
     public function change_style(Request $request, $type)
     {
         session()->put('style', $type);
@@ -28,6 +30,7 @@ class ChangeStyleController extends Controller
 
           // $result = mail::send(new SendQRTiketsToUsersEmail($generate_qr, $email));
         // }
+    $generate_qr = $this->getTokenQr(1);
         $generate_qr = TicketQr::whereIn('id',[1,2])->get();
         // dd($generate_qr);
         $email = $request->email;
