7216297579577b5f5cbdf566325fdba284b10f4d
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-05-14 11:55:08 +0400

    remove user_interface

--- routes/web.php
+++ routes/web.php
@@ -69,25 +69,6 @@ use App\Http\Controllers\authentications\LoginBasic;
 use App\Http\Controllers\authentications\RegisterBasic;
 use App\Http\Controllers\authentications\ForgotPasswordBasic;
 use App\Http\Controllers\cards\CardBasic;
-use App\Http\Controllers\user_interface\Accordion;
-use App\Http\Controllers\user_interface\Alerts;
-use App\Http\Controllers\user_interface\Badges;
-use App\Http\Controllers\user_interface\Buttons;
-use App\Http\Controllers\user_interface\Carousel;
-use App\Http\Controllers\user_interface\Collapse;
-use App\Http\Controllers\user_interface\Dropdowns;
-use App\Http\Controllers\user_interface\Footer;
-use App\Http\Controllers\user_interface\ListGroups;
-use App\Http\Controllers\user_interface\Modals;
-use App\Http\Controllers\user_interface\Navbar;
-use App\Http\Controllers\user_interface\Offcanvas;
-use App\Http\Controllers\user_interface\PaginationBreadcrumbs;
-use App\Http\Controllers\user_interface\Progress;
-use App\Http\Controllers\user_interface\Spinners;
-use App\Http\Controllers\user_interface\TabsPills;
-use App\Http\Controllers\user_interface\Toasts;
-use App\Http\Controllers\user_interface\TooltipsPopovers;
-use App\Http\Controllers\user_interface\Typography;
 use App\Http\Controllers\extended_ui\PerfectScrollbar;
 use App\Http\Controllers\extended_ui\TextDivider;
 use App\Http\Controllers\icons\Boxicons;
@@ -132,27 +113,6 @@ Route::group(['middleware' => ['auth']], function () {
 
   // cards
   Route::get('/cards/basic', [CardBasic::class, 'index'])->name('cards-basic');
-
-  // User Interface
-  Route::get('/ui/accordion', [Accordion::class, 'index'])->name('ui-accordion');
-  Route::get('/ui/alerts', [Alerts::class, 'index'])->name('ui-alerts');
-  Route::get('/ui/badges', [Badges::class, 'index'])->name('ui-badges');
-  Route::get('/ui/buttons', [Buttons::class, 'index'])->name('ui-buttons');
-  Route::get('/ui/carousel', [Carousel::class, 'index'])->name('ui-carousel');
-  Route::get('/ui/collapse', [Collapse::class, 'index'])->name('ui-collapse');
-  Route::get('/ui/dropdowns', [Dropdowns::class, 'index'])->name('ui-dropdowns');
-  Route::get('/ui/footer', [Footer::class, 'index'])->name('ui-footer');
-  Route::get('/ui/list-groups', [ListGroups::class, 'index'])->name('ui-list-groups');
-  Route::get('/ui/modals', [Modals::class, 'index'])->name('ui-modals');
-  Route::get('/ui/navbar', [Navbar::class, 'index'])->name('ui-navbar');
-  Route::get('/ui/offcanvas', [Offcanvas::class, 'index'])->name('ui-offcanvas');
-  Route::get('/ui/pagination-breadcrumbs', [PaginationBreadcrumbs::class, 'index'])->name('ui-pagination-breadcrumbs');
-  Route::get('/ui/progress', [Progress::class, 'index'])->name('ui-progress');
-  Route::get('/ui/spinners', [Spinners::class, 'index'])->name('ui-spinners');
-  Route::get('/ui/tabs-pills', [TabsPills::class, 'index'])->name('ui-tabs-pills');
-  Route::get('/ui/toasts', [Toasts::class, 'index'])->name('ui-toasts');
-  Route::get('/ui/tooltips-popovers', [TooltipsPopovers::class, 'index'])->name('ui-tooltips-popovers');
-  Route::get('/ui/typography', [Typography::class, 'index'])->name('ui-typography');
 
   // extended ui
   Route::get('/extended/ui-perfect-scrollbar', [PerfectScrollbar::class, 'index'])->name('extended-ui-perfect-scrollbar');
