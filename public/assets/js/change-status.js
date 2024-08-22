f05595cd125c47678ff3bf74679633e9c991f929
NarineHay <naromisho87@gmail.com>
2024-08-22 09:30:21 +0400

    22.08_v2

--- public/assets/js/change-status.js
+++ public/assets/js/change-status.js
@@ -19,19 +19,19 @@ $(function () {
         console.log(changeElemen.closest('tr').children('.' + field_name))
         if (data.result == 1) {
 
-          if (field_name == 'online_sales'){
+          if (field_name == 'online_sales') {
             status ? (status_word = 'Առցանց վաճառք', status_class = 'success') : (status_word = 'Առցանց վաճառք', status_class = 'danger')
           }
-          else{
+          else {
             status ? (status_word = 'Ակտիվ', status_class = 'success') : (status_word = 'Ապաակտիվ', status_class = 'danger')
           }
           // status && field_name == 'online_sales' ? (status_word = 'Ակտիվ1', status_class = 'success') : (status_word = 'Ապաակտիվ2', status_class = 'danger')
-
-          if (tb_name == 'events'){
+          console.log(status_word + '--status_word--', status_class + '--status_class --')
+          if (tb_name == 'events') {
             changeElemen.closest('tr').children('.statuses').children('.' + field_name).html(`
               <span class="badge bg-label-${status_class} me-1">${status_word}</span>`)
-          }else{
-              changeElemen.closest('tr').children('.' + field_name).html(`
+          } else {
+            changeElemen.closest('tr').children('.' + field_name).html(`
               <span class="badge bg-label-${status_class} me-1">${status_word}</span>`)
           }
 
