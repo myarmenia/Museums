c88d2e440a8cc2a880f9ce35308da209049983a3
NarineHay <naromisho87@gmail.com>
2024-08-22 16:32:40 +0400

    456

--- app/Http/Resources/API/User/ListActiveQRResource.php
+++ app/Http/Resources/API/User/ListActiveQRResource.php
@@ -42,13 +42,13 @@ class ListActiveQRResource extends JsonResource
         }
         else{
             if ($this->type == 'subscription') {
-                $data['date'] = date('d-m-Y', strtotime($this->created_at));
+                $data['date'] = date('d.m.Y', strtotime($this->created_at));
             }
             if ($this->type == 'event-config') {
-                $data['date'] = date('d-m', strtotime($this->event_config->day)) . '|' . date('H:i', strtotime($this->event_config->start_time)) . '-' . date('H:i', strtotime($this->event_config->end_time));
+                $data['date'] = date('d.m', strtotime($this->event_config->day)) . '|' . date('H:i', strtotime($this->event_config->start_time)) . '-' . date('H:i', strtotime($this->event_config->end_time));
             }
             if ($this->type == 'event') {
-              $data['date'] = date('d-m', strtotime($this->event->start_date)) . '-' . date('d-m', strtotime($this->event->end_date));
+              $data['date'] = date('d.m', strtotime($this->event->start_date)) . '-' . date('d.m', strtotime($this->event->end_date));
             }
 
             $data['museum_address'] = $this->museum->getCurrentTranslation[0]->address;
--- resources/views/layouts/mail/send-qr-ticket.blade.php
+++ resources/views/layouts/mail/send-qr-ticket.blade.php
@@ -193,8 +193,8 @@
                                   <span >AMD</span>
                                 </div>
                                 <div style="width: 50%; text-align: end; align-content: content">
-                                    <span>{{ $data->created_at != null && ($type == 'subsctiption' || $type == 'united') ? date('d-m-Y', strtotime($data->created_at)) : ''}} </span>
-                                    <span>{{ $type == 'event-config' ? date('d-m-Y', strtotime($data->purchased_item->event_config->day)) : ($type == 'event' ? date('d-m-Y', strtotime($data->purchased_item->event->start_date)) . ' - ' . date('d-m-Y', strtotime($data->purchased_item->event->end_date)) : '')}}</span>
+                                    <span>{{ $data->created_at != null && ($type == 'subsctiption' || $type == 'united') ? date('d.m.Y', strtotime($data->created_at)) : ''}} </span>
+                                    <span>{{ $type == 'event-config' ? date('d.m.Y', strtotime($data->purchased_item->event_config->day)) : ($type == 'event' ? date('d.m.Y', strtotime($data->purchased_item->event->start_date)) . '-' . date('d.m.Y', strtotime($data->purchased_item->event->end_date)) : '')}}</span>
                                     <span>{{ $type == 'event-config' ? date('H:i', strtotime($data->purchased_item->event_config->start_time)) : ''}}</span>
                                     <p style="margin-bottom: 0; font-size: 10px; color: #aaa">{{$code}}</p>
                                 </div>
