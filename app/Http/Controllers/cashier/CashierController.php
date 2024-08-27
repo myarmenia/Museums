b06cc18f9944ff4691300842e43d34156e8652d1
NarineHay <naromisho87@gmail.com>
2024-08-27 17:17:58 +0400

    564--112233

--- app/Http/Controllers/cashier/CashierController.php
+++ app/Http/Controllers/cashier/CashierController.php
@@ -130,10 +130,10 @@ class CashierController extends Controller
                 'guid' => $purchaseGuid? $purchaseGuid : false,
                 'price' => $qr['price'],
                 'created_at' => $qr['created_at'],
-                'sub_type' => $qr['type'] == 'event-config' ? $qr->event_config->event->sub_type : ($qr['type'] == 'event' ? $qr->event->sub_type : null)
+                'sub_type' => $qr->purchased_item->sub_type
             ];
 
-            
+
         }
 
         $pdf = Pdf::loadView('components.ticket-print', ['tickets' => $data])->setPaper([0, 0, 300, 600], 'portrait');
--- resources/views/components/ticket-print.blade.php
+++ resources/views/components/ticket-print.blade.php
@@ -63,7 +63,7 @@
                     <span>Տեսակ - </span>
                     <span>&nbsp;{{ getTranslateTicketTitl($item['type']) }}</span>
                 </div>
-                @if (isset($item['sub_type']))
+                @if (isset($item['sub_type']) && $item['sub_type'] != null)
                     <div class="text-flex text-margin">
                         <span>Տոմսի Տեսակ - </span>
                         <span>&nbsp;{{ $item['sub_type'] == 'standart' ? 'Ստանդարտ' : ( $item['sub_type'] == 'discount' ? 'Զեղչված' : 'Անվճար' )}}</span>
