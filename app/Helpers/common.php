ef7d2559310b018c8ad06a101c5e9b97ea093f3e
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-08-30 17:24:14 +0400

    add en

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -404,7 +404,7 @@ if (!function_exists('getTranslateTicketTitl')) {
   }
 
 }
-if (!function_exists('getTranslateTicketTitl')) {
+if (!function_exists('getTranslateTicketTitl_en')) {
   function getTranslateTicketTitl_en($title)
   {
       $titles = [
