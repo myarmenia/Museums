f99d6377d95b53f11438aadd246da725659704b0
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-08-30 11:33:44 +0400

    add all price to museum/event

--- app/Http/Resources/API/Tickets/EventsListViaTicketsResource.php
+++ app/Http/Resources/API/Tickets/EventsListViaTicketsResource.php
@@ -15,6 +15,20 @@ class EventsListViaTicketsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+          $price_array[0]=[
+
+            "price"=>$this->price,
+            "sub_type"=>'standart'
+
+        ];
+        if($this->discount_price!=null){
+
+          $price_array[1]=[
+            "price"=>$this->discount_price,
+            "sub_type"=>'discount'
+          ];
+        }
+
 
         $configs = $this->event_configs;
         $configs = isset($request->start_date) ? $configs->where('day', '>=', $request->start_date) : $configs;
@@ -28,6 +42,7 @@ class EventsListViaTicketsResource extends JsonResource
           'start_date' => date('d-m-Y', strtotime($this->start_date)),
           'end_date' => date('d-m-Y', strtotime($this->end_date)),
           'price' => $this->price,
+          'all_prices'=>$price_array,
           'style' => $this->style
 
         ];
