be912f546570620ca6fb6fffd89aba51ddaf0a4f
NarineHay <naromisho87@gmail.com>
2024-09-16 09:17:00 +0400

    16.09

--- app/Services/Log/LogService.php
+++ app/Services/Log/LogService.php
@@ -11,14 +11,13 @@ class LogService
   public static function store(array|null $data, $tb_id, $tb_name, $type)
   {
 
-      // $user = Auth::user();
+      $user = Auth::user();
 
       $data = $data ? json_encode($data, JSON_UNESCAPED_UNICODE) : null;
 
       $log = Log::create([
-        // 'user_id' => $user->id,
-      'user_id' => 1,
-      'type' => $type,
+        'user_id' => $user->id,
+        'type' => $type,
         'tb_name' => $tb_name,
         'tb_id' => $tb_id,
         'data' => $data
