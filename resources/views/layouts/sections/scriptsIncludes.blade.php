47b9838d178dd8976bea66718a547dd2620c014e
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-09-20 17:51:13 +0400

    working logout

--- resources/views/layouts/sections/scriptsIncludes.blade.php
+++ resources/views/layouts/sections/scriptsIncludes.blade.php
@@ -3,8 +3,32 @@
 
 <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 <script src="{{ asset('assets/js/config.js') }}"></script>
+<script>
+  let window_close_logout_url ="{{ route('logout') }}"
 
-{{-- <script src="{{ asset('assets/js/admin/turnstile-managment.js') }}"></script> --}}
+    window.addEventListener('beforeunload', async (e) =>{
+
+        try {
+          const res = await fetch(window_close_logout_url, {
+            method: "POST",
+            body: JSON.stringify({
+                token: 'true'
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        } catch (error) {
+          console.log(error);
+
+        }
+
+    });
+
+</script>
+
+
+{{-- <script src="{{ asset('assets/js/admin/turnstile-managment.js') }}"></> --}}
 
 <!-- Place this tag in your head or just before your close body tag. -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
