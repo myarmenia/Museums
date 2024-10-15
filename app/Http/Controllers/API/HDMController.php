fa4beec10767695d774af05173251a8821465883
NarineHay <naromisho87@gmail.com>
2024-10-16 01:00:53 +0400

    444

--- app/Http/Controllers/API/HDMController.php
+++ app/Http/Controllers/API/HDMController.php
@@ -10,40 +10,77 @@ class HDMController extends Controller
   public function __invoke()
   {
 
-      $host = '192.168.10.126';
+      $host = '192.168.1.157';
       $port = 8080;
       $password = 'ReVZh4PJ';
 
       // Создаем TCP-соединение
       $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
+      if (!$socket) {
+        dd(11);
+          die("Couldn't create socket: " . socket_strerror(socket_last_error()));
+      }
       if ($socket === false) {
         dd(66);
         echo "Ошибка создания сокета: " . socket_strerror(socket_last_error()) . "\n";
       } else {
+        if (socket_connect($socket, $host, $port)) {
+        // 12-բայթ գլխագրի կառուցում
+          $id = 1;  // ID, օրինակ՝ 1
+          $type = 2;  // Տվյալների տեսակ, օրինակ՝ 2
+          $dataLength = 100;  // Տվյալների երկարությունը, օրինակ՝ 100 բայթ
 
-        $result = socket_connect($socket, $host, $port);
+          // Գլխագրի փաթեթավորում որպես 12-բայթ բինար տվյալ
+          $header = pack('N*', $password);
 
-        if ($result === false) {
-          dd(22);
-          echo "Ошибка подключения: " . socket_strerror(socket_last_error($socket)) . "\n";
+            // Ուղարկել 12-բայթ գլխագիրը
+            // $sent = socket_write($socket, $header, strlen($header));
+            // if ($sent === false) {
+            //     die("Socket write error: " . socket_strerror(socket_last_error($socket)));
+            // }
 
-        } else {
-          // $command = "COMMAND_HDM";
-        // $command = json_encode([
-        //   'password' => $password,
-        //   // 'deviceId' => '123456'
-        // ]);
-        $command = "GET_STATUS";
-          socket_write($socket, $command, strlen($command));
+            // echo "12-բայթ գլխագիրը ուղարկվեց հաջողությամբ";
 
-          // Чтение ответа
-          $response = socket_read($socket, 1024);
-          echo "Ответ от устройства: $response\n";
-        }
-        dd(44);
-        socket_close($socket);
+            $data = json_encode([
+              "password" => "ReVZh4PJ",
+              "cashier"=>3,
+              "pin"=>3
+          ]);
+          $key = $this->generateKey($password);
+
+          $encryptedData = $this->encryptData($data, $key);
+          $p = socket_write($socket, $encryptedData, strlen($encryptedData));
+            // Փակել սոկետը
+            // socket_close($socket);
+
+            $response = socket_read($socket, 2048);  // ստանում ենք պատասխանը
+
+            if ($response === false) {
+              die("Unable to read from socket: " . socket_strerror(socket_last_error($socket)));
+          }
+            $decryptedResponse = decryptData($response, $key);  // ապակոդավորում ենք պատասխանը
+            dd($decryptedResponse);
       }
+    }
 dd('finish');
   }
+
+  function encryptData($data, $key) {
+    // PKCS7 padding-ի համար կոդավորվող տվյալները բլոկի չափի համաձայն
+    $blockSize = 8;  // 3DES բլոկի չափը
+    $pad = $blockSize - (strlen($data) % $blockSize);
+    $data .= str_repeat(chr($pad), $pad);
+
+    // 3DES կոդավորում օգտագործելով ECB ռեժիմ
+    $encrypted = openssl_encrypt($data, 'DES-EDE3', $key, OPENSSL_RAW_DATA | OPENSSL_NO_PADDING);
+
+    return $encrypted;
+}
+
+function generateKey($password) {
+  // Գեներացնում ենք 24-բայթանոց բանալի ՀԴՄ գաղտնաբառի հիման վրա
+  $hash = hash('sha256', $password, true);
+  return substr($hash, 0, 24); // Վերցնում ենք առաջին 24 բայթերը
+}
+
 }
