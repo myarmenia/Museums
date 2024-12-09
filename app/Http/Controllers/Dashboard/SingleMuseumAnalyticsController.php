f42597963360a5cbf96c55dfd95f571f0147963a
NarineHay <naromisho87@gmail.com>
2024-12-09 15:33:13 +0400

    2252

--- app/Http/Controllers/Dashboard/SingleMuseumAnalyticsController.php
+++ app/Http/Controllers/Dashboard/SingleMuseumAnalyticsController.php
@@ -20,15 +20,15 @@ class SingleMuseumAnalyticsController extends Controller
       $ticket_type = $this->ticketsType($museum_id);
 
       // Оптимизация: вызов groupAgeForAllMuseum один раз
-      $group_age_data = $this->groupAgeForAllMuseum($museum_id);
-      $attendance_by_age_arr = json_encode($group_age_data['age_analytics']);
-      $age_total_amount = $group_age_data['age_total_amount'];
-      $age_total_amount_asoc_arr = $group_age_data['age_analytics_asoc_arr'];
+      // $group_age_data = $this->groupAgeForAllMuseum($museum_id);
+      // $attendance_by_age_arr = json_encode($group_age_data['age_analytics']);
+      // $age_total_amount = $group_age_data['age_total_amount'];
+      // $age_total_amount_asoc_arr = $group_age_data['age_analytics_asoc_arr'];
 
       // // Оптимизация: вызов forAllMuseum один раз
-      $attendance_by_country_data = $this->forAllMuseum($museum_id);
-      $attendance_by_country = json_encode($attendance_by_country_data['statistics']);
-      $attendance_by_country_arr = $attendance_by_country_data['array'];
+      // $attendance_by_country_data = $this->forAllMuseum($museum_id);
+      // $attendance_by_country = json_encode($attendance_by_country_data['statistics']);
+      // $attendance_by_country_arr = $attendance_by_country_data['array'];
 
       // // Аналитика по месяцам
       $analitics_by_month = json_encode($this->analyticsByMonth($museum_id));
@@ -39,7 +39,15 @@ class SingleMuseumAnalyticsController extends Controller
     // return view("content.dashboard.museum-dashboards-analytics", compact('group_age_data'));
 
 
-    return view("content.dashboard.museum-dashboards-analytics", compact('ticket_type', 'analitics_by_month', 'attendance_by_age_arr', 'age_total_amount', 'age_total_amount_asoc_arr', 'attendance_by_country', 'attendance_by_country_arr'));
+    return view("content.dashboard.museum-dashboards-analytics", compact(
+      'ticket_type',
+      'analitics_by_month',
+      // 'attendance_by_age_arr',
+      // 'age_total_amount',
+      // 'age_total_amount_asoc_arr',
+      // 'attendance_by_country',
+      // 'attendance_by_country_arr'
+    ));
 
   }
 }
