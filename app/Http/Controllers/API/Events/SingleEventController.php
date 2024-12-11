f2404cd3eec0bd060b4c2fffe7761b568e2742f3
NarineHay <naromisho87@gmail.com>
2024-12-11 12:48:09 +0400

    11.12.24

--- app/Http/Controllers/API/Events/SingleEventController.php
+++ app/Http/Controllers/API/Events/SingleEventController.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\API\BaseController;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\API\Events\SingleEventResource;
 use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class SingleEventController extends BaseController
 {
     public function __invoke($event_id){
 
-      $single_event = Event::where(['id' => $event_id, 'status' => 1, 'online_sales' => 1])->first();
+      $single_event = Event::where(['id' => $event_id, 'status' => 1, 'online_sales' => 1])->whereDate('end_date', '>=', Carbon::today())->first();
       return $this->sendResponse(new SingleEventResource($single_event),'success');
 
     }
