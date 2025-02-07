054dc0a6d671b6f88aa4cdf7301680fecd3da66d
NarineHay <naromisho87@gmail.com>
2025-02-07 14:07:11 +0400

    10:30 1

--- app/Console/Commands/DeleteOldPdfs.php
+++ app/Console/Commands/DeleteOldPdfs.php
@@ -36,7 +36,7 @@ class DeleteOldPdfs extends Command
         do {
             // Getting 500 old files
             $oldPdfs = TicketPdf::where('created_at', '<', $now->copy()->subMonths(3))->limit($batchSize)->get();
-$this->info($now->copy()->subMonths(3));
+
             if ($oldPdfs->isEmpty()) {
               break;
             }
